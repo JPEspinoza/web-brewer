13c21ad54875e12d83037877fa222c3531a507f7
Juan Pablo Espinoza <juaespinoza@icloud.com>
2019-04-21 16:21:10 -0400

    half baked the language switcher

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 const contentArea = document.getElementById("page");
-console.log(contentArea);
+
+//name of navs in both languages
+const dict= {
+    "es": {
+        0: "Nav 1 es",
+        1: "Nav 2 es",
+        2: "Nav 3 es"
+    },
+    "en": {
+        0: "Nav 1 en",
+        1: "Nav 2 en",
+        2: "Nav 3 en"
+    }
+}
+
+var languages = ["en", "es"]; //first language is current
+
+console.log(dict["es"][1]);
+
+function setLanguage() 
+{
+    //swap languages
+    var temp = languages[0];
+    languages[0] = languages[1];
+    languages[1] = temp;
+
+    var divs = document.getElementsByClassName("nav-button");
+
+    for(i = 0; i < 3; i++)
+    {
+        divs[i].innerHTML = dict[languages[0]][i];
+    }
+    resetButtons();
+}
 
 function resetButtons() 
 {
@@ -28,7 +61,9 @@ function httpGet(myUrl, callback)
 function activatePage(navbutt, source) 
 {
     resetButtons();
-    console.log("click");
+    
+    var load = "pages/" + language + "/" + source + ".html";
+
     navbutt.className += " active";
     httpGet(source, setContent);
 }
@@ -37,4 +72,8 @@ function setContent(payload)
 {
     contentArea.innerHTML = payload;
 }
+
+//set default language
+setLanguage();
+//default clicked element
 document.getElementById("initial").click();
